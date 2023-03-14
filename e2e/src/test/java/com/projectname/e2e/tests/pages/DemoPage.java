74fc08315de5982e94e6e34dab863acf97337249
Verica <verica.bojovic@quinyx.com>
2023-03-14 10:38:02 +0100

    e2e tests

---
+++ e2e/src/test/java/com/projectname/e2e/tests/pages/DemoPage.java
@@ -0,0 +1,29 @@
+package com.projectname.e2e.tests.pages;
+
+import com.projectname.e2e.tests.pages.common.PageBase;
+import com.projectname.e2e.tests.selectors.CustomBy;
+import com.projectname.e2e.tests.utils.CheckIfElement;
+import com.projectname.e2e.tests.webdriver.CustomWebDriver;
+
+public class DemoPage extends PageBase {
+
+    public DemoPage(CustomWebDriver driver, String url, String email, String password) {
+        super(driver, url, email, password);
+    }
+
+    @Override
+    public PageBase show() {
+        if(!isDisplayed()) {
+            NavigationBarPage navigationBarPage = new NavigationBarPage(driver, url, email, password);
+            if(navigationBarPage.isDisplayed()) {
+                navigationBarPage.openDemoPage();
+            }
+        }
+        return this;
+    }
+
+    @Override
+    public boolean isDisplayed() {
+        return CheckIfElement.isDisplayed(CustomBy.xpath("//*[@id=\"content\"]/section[1]/div/div/div[2]/div/div/div"), driver);
+    }
+}
---
+++ e2e/src/test/java/com/projectname/e2e/tests/pages/FeaturesPage.java
@@ -0,0 +1,23 @@
+package com.projectname.e2e.tests.pages;
+
+import com.projectname.e2e.tests.pages.common.PageBase;
+import com.projectname.e2e.tests.selectors.CustomBy;
+import com.projectname.e2e.tests.utils.CheckIfElement;
+import com.projectname.e2e.tests.webdriver.CustomWebDriver;
+
+public class FeaturesPage extends PageBase {
+
+    public FeaturesPage(CustomWebDriver driver, String url, String email, String password) {
+        super(driver, url, email, password);
+    }
+
+    @Override
+    public PageBase show() {
+        return null;
+    }
+
+    @Override
+    public boolean isDisplayed() {
+        return CheckIfElement.isDisplayed(CustomBy.xpath("/html/body/div[2]/div/div[3]/ul/li[3]/div/ul/li[2]/a/span"), driver);
+    }
+}
